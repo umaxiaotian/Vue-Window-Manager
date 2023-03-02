08177f7a2f05eb6cf5d91fac3ad329bba8176cf3
umaxiaotian <yuma@umaxiaotian.com>
2023-03-03 04:17:18 +0900

    不要なものを削除

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+export default ({
   transpileDependencies: true,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'vue-window-manager': 'vue-window-manager/dist/your-package-name.esm.js'
-      }
-    }
-  }
+  
 })
