5ee6f0c3144fddd025db1b9a3ffa26295de7215e
umaxiaotian <yuma@umaxiaotian.com>
2023-03-03 01:52:04 +0900

    FIX エントリポイントエラー

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import vueWindowManager from './components/vueWindowManager.vue'
-
-export default {
-  install: (app) => {
-    app.component(vueWindowManager.name, vueWindowManager)
-  }
-}
-
 export { vueWindowManager }
