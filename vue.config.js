007ccb39c42f1c11a07ec56b8d1a8bb4fa86f5a2
umaxiaotian <yuma@umaxiaotian.com>
2023-03-03 02:05:22 +0900

    モジュールを使うと「 Failed to resolve entry for package "vue-window-manager". The package may have incorrect main/module/exports specified in its package.json.」と表示る問題を修正

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue-window-manager': 'vue-window-manager/dist/your-package-name.esm.js'
+      }
+    }
+  }
 })
